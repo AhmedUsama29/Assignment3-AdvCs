429a5087c0efe4095c759298e496740cc6aca920
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-02-04 14:51:08 +0200

    Q1

--- Assignment3 AdvC#/Program.cs
+++ Assignment3 AdvC#/Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1 : Given a Queue, implement a function to reverse the elements of a queue using a stack.
+
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(10);
+            queue.Enqueue(20);
+            queue.Enqueue(30);
+
+            Console.WriteLine("-----------Before------------");
+            foreach (int item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            ReverseQueue(queue);
+
+            Console.WriteLine("-----------After------------");
+
+            foreach (int item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            #endregion
+
+
+
         }
+
+        #region Q1 : Given a Queue, implement a function to reverse the elements of a queue using a stack.
+        public static void ReverseQueue<T>(Queue<T> queue)
+        {
+            Stack<T> stack = new Stack<T>();
+
+            while (queue.Count > 0)
+            {
+                stack.Push(queue.Dequeue());
+            }
+
+            while (stack.Count > 0)
+            {
+                queue.Enqueue(stack.Pop());
+            }
+
+        } 
+        #endregion
     }
 }
